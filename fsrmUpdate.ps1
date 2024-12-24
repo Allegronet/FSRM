64bb769b943f24aa03d4ac283ef74afb30ef5045
Allegronet <142716607+Allegronet@users.noreply.github.com>
2024-12-24 11:43:48 +0200

    Create fsrmUpdate.ps1
    v1

---
+++ fsrmUpdate.ps1
@@ -0,0 +1,8 @@
+try {
+    Set-FsrmFileGroup -Name "CryptoWall File Monitor" -IncludePattern (
+        (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Allegronet/FSRM/refs/heads/main/fsrm-block-lost.txt" -UseBasicParsing).Content -split "`r?`n" |
+        Where-Object { $_ -ne "" }
+    )
+} catch {
+    Write-Output $_ | Out-File -FilePath "C:\TaskLogs\FSRM-Error.log"
+}
